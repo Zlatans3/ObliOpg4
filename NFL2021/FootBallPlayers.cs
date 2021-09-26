da2f3e434f2bdc1215b15a4e68123130a5a893c7
Zlatan Stadler <zlat0179@edu.easj.dk>
2021-09-26 19:20:37 +0200

    Add project files.

---
+++ NFL2021/FootBallPlayers.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace FootBallPlayer
+{
+    public class FootBallPlayer
+    {
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length > 4) throw new ArgumentOutOfRangeException();
+                _name = value;
+            }
+        }
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException();
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value < 1 || value > 100) throw new ArgumentOutOfRangeException();
+                _shirtNumber = value;
+            }
+        }
+
+        public FootBallPlayer()
+        {
+
+        }
+
+        public FootBallPlayer(int id, string name, int price, int shirtNumber)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Price)}: {Price}, {nameof(ShirtNumber)}: {ShirtNumber}";
+        }
+    }
+}
